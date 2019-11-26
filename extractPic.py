30352b126bc7cf70f9bd926b31bf54a702e4aa8e
“rainyforest” <rainyforest4869@gmail.com>
2019-11-25 19:26:14 -0800

    Nov 25

--- extractPic.py
+++ extractPic.py
@@ -6,6 +6,7 @@ from sklearn.svm import LinearSVC
 
 size_record = 2952
 
+
 def read_record(f):
     pixels = []
     s = f.read(size_record)
@@ -23,74 +24,82 @@ def read_record(f):
 
 
 def read_hiragana():
-    # 51: number of katakana characters
+    # 48: number of katakana characters
     # 208: number of images for each katakana
     # 50*50: image size
-    arr = np.zeros([51,208, 50,50], dtype=np.float32)
+    arr = np.zeros([48,208,50,50], dtype=np.float32)
     filename = 'ETL5C'
     with open(filename, 'rb') as f:
-        for i in range(10608):
+        i = 0
+        char_offset = 0
+        while i < 10608:
         # for i in range(500):
             pixels_arr = np.array(read_record(f))
             pic = np.reshape(pixels_arr,(76,72)).astype(np.float32)
             pic = pic[10:60,10:60]
-            # plt.imshow(pic, cmap='gray', interpolation='nearest')
-            # plt.savefig("pic{:04d}.png".format(i))
-            # print("{:04d} finished.".format(i))
-            #
-
-                # plt.show()
-            # if i%208==0:
-            #     plt.imshow(pic, cmap='gray', interpolation='nearest')
-            #     plt.savefig("pic{:04d}.png".format(math.floor(i/208)))
-            arr[math.floor(i/208),i%208]= pic
+            if math.floor(i/208) in [36,38,47]:
+                i = i + 208
+                char_offset = char_offset + 1
+                continue
+            arr[math.floor(i/208)-char_offset,i%208]= pic
+            i = i + 1
     return arr
 
 def training(arr):
     # prepare X and y
-    train_sz = 100
+    train_sz = 198
     test_sz = 10
-    X = np.zeros([train_sz*51, 50*50], dtype=np.float32)
-    Xte = np.zeros([test_sz*51, 50*50], dtype=np.float32)
+    X = np.zeros([train_sz*48, 50*50], dtype=np.float32)
+    Xte = np.zeros([test_sz*48, 50*50], dtype=np.float32)
 
     # get training matrix X, training label y, testing matrix Xte, testing label yte
-    for i in range(51):
+    for i in range(48):
          train_pics = arr[i,0:train_sz,:,:]
          train_tuples = np.reshape(train_pics,(train_sz,50*50))
-         X[i*train_sz:(i+1)*train_sz,:] = train_tuples
+         X[i*train_sz:(i+1)*train_sz , :] = train_tuples
 
          test_pics = arr[i,train_sz:train_sz+test_sz,:,:]
          test_tuples = np.reshape(test_pics,(test_sz,50*50))
          Xte[i*test_sz:(i+1)*test_sz,:] = test_tuples
 
-    y = np.zeros([train_sz*51])
-    yte = np.zeros([test_sz*51])
+    y = np.zeros([train_sz*48])
+    yte = np.zeros([test_sz*48])
 
-    for i in range(train_sz*51):
+    for i in range(train_sz*48):
         y[i] = math.floor(i/train_sz)
 
-    for i in range(test_sz*51):
+    for i in range(test_sz*48):
         yte[i] = math.floor(i/test_sz)
-    print(Xte)
+    linearSVC(X,y,Xte,yte)
+
+
+def comp(Xte_res,yte):
+    match_num = 0
+    for i in range(len(Xte_res)):
+        if Xte_res[i] == yte[i]:
+            match_num = match_num + 1
+        print("{} ------ {}".format(Xte_res[i],yte[i]))
+    print("All {} test samples, {} matches. Predict rate is {}.".format(len(yte),match_num,match_num/len(yte)))
+
+
+def linearSVC(X,y,Xte,yte):
     clf = LinearSVC()
     clf.fit(X, y)
-
     Xte_res = clf.predict(Xte)
-
+    comp(Xte_res,yte)
     #print out predict results and compare
-    for i in range(len(yte)):
-        print("{} ------ {}".format(Xte_res[i],yte[i]))
 
 
 
-         # plt.imshow(samples[100], cmap='gray', interpolation='nearest')
-         # plt.show()
-#             plt.savefig("pic{:04d}.png".format(i)) #save image as file
-#             print("{:04d} finished.".format(i))
+def save_image(pic,idx):
+    plt.imshow(samples[100], cmap='gray', interpolation='nearest')
+    plt.savefig("pic{:04d}.png".format(idx)) #save image as file
+    print("{:04d} finished.".format(idx))
 
 def main():
     arr = read_hiragana()
     training(arr)
+
 
 if __name__ == "__main__":
     main()
